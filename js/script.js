25892634dbd800b4503e9bd35b081b129ae7b969
Julie_S <juliesaltrelli@gmail.com>
2021-11-16 17:36:42 -0800

    Step 1 and begining of step 2

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Create Global Variables
+
+ guessedLetters = document.querySelector(".guessed-letters");
+const button = document.querySelector(".guess");
+const textInput = document.querySelector(".letter");
+const inProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesDisplay = document.querySelector(".remaining"); //span?
+const messages = document.querySelector(".message");
+const hiddenButton = document.querySelector(".guess");
+const word = "magnolia"; 
+
+//Write a Function to Add Placeholders for Each Letter
+
+const wordsInProgress = function () {
+    if (guessedLetters 
+    inProgress.innerText = "●"
+}
+
+wordsInProgress(word);
