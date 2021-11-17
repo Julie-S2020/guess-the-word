23167f3e159d5c98b7eb075d5f02a453d8196e59
Julie_S <juliesaltrelli@gmail.com>
2021-11-17 08:08:47 -0800

    Write a Function to Add Placeholders for Each Letter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 //Create Global Variables
 
- guessedLetters = document.querySelector(".guessed-letters");
+const guessedLetters = document.querySelector(".guessed-letters");
 const button = document.querySelector(".guess");
 const textInput = document.querySelector(".letter");
-const inProgress = document.querySelector(".word-in-progress");
+const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
-const remainingGuessesDisplay = document.querySelector(".remaining"); //span?
+const remainingGuessesDisplay = document.querySelector(".remaining span"); 
 const messages = document.querySelector(".message");
-const hiddenButton = document.querySelector(".guess");
+const platAgainButton = document.querySelector(".play-again");
 const word = "magnolia"; 
 
 //Write a Function to Add Placeholders for Each Letter
 
-const wordsInProgress = function () {
-    if (guessedLetters 
-    inProgress.innerText = "●"
-}
+const wordToGuess = function (word) {
+    const wordToGuessLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+       wordToGuessLetters.push("●"); 
+    }
+    
+   wordInProgress.innerText = wordToGuessLetters.join("");    
+};
 
-wordsInProgress(word);
+wordToGuess(word);
